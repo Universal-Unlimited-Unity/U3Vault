af74837c4907fe201db64bb94f4dff9d718c994b
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-08 05:03:30 +0000

    Attendance table

--- backend/hr/Attendance/db_manage.py
+++ backend/hr/Attendance/db_manage.py
@@ -1,10 +1,32 @@
-from sqlalchemy import MetaData, Table, Column
+from sqlalchemy import MetaData, Table, Column, ForeignKey, String, Date, select
 from db.db_connect import db_connect
+
 eng = db_connect()
 
 metadata = MetaData()
 Attendance = Table(
     "Attendance",
     metadata,
-    Column("id", UUID(as_uuid=True), primary_key=True)
+    Column("id", UUID(as_uuid=True), ForeignKey("employees.id", ondelete="SET NULL"),
+           nullable=True),
+    Column("first_name", String, nullable=False),
+    Column("middle_name", String, nullable=True),
+    Column("last_name", String, nullable=False),
+    Column("date", Date, nullable=False)
+    Column("status", String, nullable=False)
 )
+
+def init() -> None:
+    metadata.create_all(eng, ckeckfirst=True)
+
+def emp_attendance_dict():
+    with eng.connect() as conn:
+        emps = select(employees).mappings().all()
+        if not emps:
+            return 1
+        newhash = {}
+        for i in emps:
+            middle_name = i["middle_name"] if emp["middle_name"] else ""
+            newhash[str(i["id"])] = {"first_name": i["first_name"], "middle_name": middle_name, "last_name": i["last_name]}
+        return newhash
+        
