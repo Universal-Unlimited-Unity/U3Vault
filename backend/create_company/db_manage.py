a11097a571fd542298a0f4b616311423cc9d73ed
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-16 02:16:02 +0000

    Enhance company table with new fields and types

--- backend/create_company/db_manage.py
+++ backend/create_company/db_manage.py
@@ -1,26 +1,28 @@
-from sqlalchemy import Table, Column, String, MetaData, insert
+from sqlalchemy import Table, Column, String, MetaData, insert, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from .model import Company
 from db.db_connect import db_connect
+from datetime import datetime
 
 eng = db_connect()
 metadata = MetaData()
 
-company = Table(
+company_table = Table(
     "company",
     metadata,
     Column("id", UUID(as_uuid=True), primary_key=True),
     Column("email", String, nullable=False, unique=True),
     Column("password", String, nullable=False),
-    Column("address", String, nullable=True),
-    Column("phone_number", String, nullable=True),
+    Column("address", String | None, nullable=True),
+    Column("phone_number", String | None, nullable=True),
+    Column("slug", String, nullable=False, unique=True),
+    Column("created_at", DateTime, default=datetime.utcnow),
 )
 
-def init()
+def init():
     metadata.create_all(eng, checkfirst=True)
-    
+
 def insert_company(company: Company):
-    
     with eng.connect() as conn:
         conn.execute(
             company_table.insert().values(
@@ -29,8 +31,9 @@ def insert_company(company: Company):
                 password=company.password,
                 address=company.address,
                 phone_number=company.phone_number,
+                slug=company.slug,
+                created_at=datetime.utcnow()
             )
         )
         conn.commit()
         return company.id
-    
