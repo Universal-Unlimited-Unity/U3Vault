23cfbd829d7dc74eadabcaae4b6a9a18e4b7892c
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-16 02:03:09 +0000

    Add slug and created_at fields to Company model

--- backend/create_company/model.py
+++ backend/create_company/model.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, EmailStr, Field
 from uuid import UUID, uuid4
-from typing import Optional
+from datetime import datetime
 
 class Company(BaseModel):
     id: UUID = Field(default_factory=uuid4)
@@ -8,4 +8,5 @@ class Company(BaseModel):
     password: str
     address: str | None
     phone_number: str | None
-    
+    slug: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
