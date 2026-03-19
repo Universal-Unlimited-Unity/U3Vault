39a47b1d0e086eaa211b2d6318ad5282f53c6661
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-19 03:15:51 +0000

    Implement authentication functions for admin and registration

---
+++ backend/auth/db_manage.py
@@ -0,0 +1,50 @@
+from sqlalchemy import select
+from passlib_context import CryptContext
+from db.db_connect import db_connect
+from datetime import datetime, timedelta
+from jose import jwt
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+pwd_context = CryptContext(shemes=["bcrypt"], deprecated="auto")
+eng = db_connect()
+
+TOKEN_EXP_MIN = os.getenv("TOKEN_EXP_MIN")
+TOKEN_KEY = os.getenv("TOKEN_KEY")
+ALGO = os.getenv("ALGO")
+
+def admin_auth(email: str, pwd: str):
+  with eng.connect() as conn:
+    stmt = select(company).where(company.c.email == email)
+    company = conn.execute(stmt).fetchone()
+    if not company:
+      return -1
+    if not pwd_context.verify(password, company.password)
+      return 1
+    payload = {
+      "role": "admin",
+      "company_id": str(company.id),
+      "exp": datetime.utcnow() + timedelta(minutes=int(TOKEN_EXP_MIN))
+      }
+    return jwt.encode(payload, TOKEN_KEY, algorithme=ALGO)
+
+def reg_auth(slug: str, email: str, pwd: str, role):
+  with eng.connect() as conn:
+    stmt = select(company.c.id).where(company.c.slug == slug)
+    comp_id = conn.execute(stmt).fetchone()
+    if not comp_id:
+      return -1
+    stmt = select(employees).where(employees.c.company_id == comp_id.id, employees.c.email == email)
+    user = conn.execute(stmt).fetchone()
+    if not user:
+      return -1
+    if not pwd_context.verify(password, user.password):
+      return 1
+    payload = {
+      "id": str(user.id),
+      "role": user.role,
+      "company_id": str(user.company_id),
+      "exp": datetime.utcnow() + timedelta(minutes:int(TOKEN_EXP_MIN))
+      }
+    return jwt.encode(payload, TOKEN_KEY, algorithme=ALGO)
