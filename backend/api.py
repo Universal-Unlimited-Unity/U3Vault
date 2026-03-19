8e72d750f927eb36221bd771d78410de65381d56
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-19 23:05:46 +0000

    Initialize FastAPI app with routers and lifespan
    Set up FastAPI application with routers and database initialization.

---
+++ backend/api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from create_company.api_manage import router as company_router
+from auth.api_manage import router as auth_router
+from attendance.api_manage import router as attendance_router
+from employees.api_manage import router as employees_router
+from contextlib import asynccontextmanager
+from create_company.db_manage import init as comp_init
+from employees.db_manage import init as emp_init
+from attendance.db_manage import init as att_init
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  print("Database Started Seccussfully")
+  comp_init()
+  emp_init()
+  att_init()
+  yield
+  print("Shutting Down...")
+  
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(company_router)
+app.include_router(auth_router)
+app.include_router(attendance_router)
+app.include_router(employees_router)
