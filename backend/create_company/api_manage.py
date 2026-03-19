8d8f83a94a884cf4a5647d81857953601a06f00e
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-19 02:31:09 +0000

    Add slug generation to create company API

--- backend/create_company/api_manage.py
+++ backend/create_company/api_manage.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from .db_manage import insert_company
+from .db_manage import insert_company, check_slug, generate_slug
 from .model import Company
 from passlib_context import CryptContext
 
@@ -8,6 +8,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 router = APIRouter(prefix="/company", tags=["company"])
 @router.post("/")
 async def create_company_api(company: Company):
+    company.slug = generate_slug(company.name)
     company.password = pwd_context.hash(company.password)    
     company_id = insert_company(company)
     if not company_id:
