8a2583f516ef526e6d7e5cd6c65044523eb3de58
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-19 23:11:07 +0000

    Update run_apis.sh to run new API script

--- backend/run_apis.sh
+++ backend/run_apis.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-fastapi dev hr/employees/api_manage.py --host 0.0.0.0 --port 8000 &
-fastapi dev hr/Attendance/api_manage.py --host 0.0.0.0 --port 8001
-wait 
+fastapi dev api/api.py --host 0.0.0.0 --port 8000
+wait
