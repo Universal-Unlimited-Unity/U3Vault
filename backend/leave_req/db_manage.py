7cbbf8b7a187778477dc7e398dc49fd4f7fb3faf
ADAM AAKIF <adamaakif23@gmail.com>
2026-03-25 20:29:25 +0100

    Add database management for leave requests
    Implemented database management for leave requests, including initialization and request addition functions.

---
+++ backend/leave_req/db_manage.py
@@ -0,0 +1,33 @@
+from db.db_connect import db_connect, metadata
+from .model import request
+from sqlalchemy import insert, Table, Column, String, Date, select, delete, ForeignKey, UniqueConstraint
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+from create_company.db_manage import company
+from employees.db_manage import employees
+
+eng = db_connect()
+
+request = Table(
+  "request",
+  metadata,
+  Column("req_id", UUID(as_uuid=True), primary_key=True, nullable=False),
+  Column("emp_id", UUID(as_uuid=True), ForeignKey("employees.id"), nullable=False),
+  Column("cmp_id", UUID(as_uuid=True), ForeignKey("employees.id"), nullable=False),
+  Column("reason", String, nullable=False),
+  Column("doc", String, nullable=True),
+  Column("status", String, nullable=False)
+)
+
+def init():
+  metadata.create_all(eng, checkfirst=True)
+
+def add_req(request: request):
+  with eng.connect() as conn:
+    stmt = insert(request).values(request.model_dump())
+    conn.execute(stmt)
+    conn.commit()
+
+def get_req_by_status(status:str):
+  with eng.connect() as conn:
+    stmt = select(request). // i will finish later
