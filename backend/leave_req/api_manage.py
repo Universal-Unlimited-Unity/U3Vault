11b993ddd66a414e6d2181755c1bce5760e7d7ba
ADAM AAKIF <adamaakif23@gmail.com>
2026-04-04 16:45:01 +0100

    Add UUID import and new endpoint for managers

--- backend/leave_req/api_manage.py
+++ backend/leave_req/api_manage.py
@@ -3,6 +3,7 @@ from .model import request
 from .db_manage import get_req_by_status, add_req
 from shared.func import lazy 
 from typing import Annotated
+import uuid
 router = APIRouter(prefix="/requests", tags=["requests"])
 
 @router.post("")
@@ -18,6 +19,8 @@ async def get_req(status : Annotated[str, Query()], auth: Annotated[str, Header(
   user = lazy(auth)
   if user["role"] == "Employee":
     return get_req_by_status(status, user["id"])
-  
-    
-  
+
+@router.get("/AdMan")
+async def get(auth: Annotated[str, Header()]):
+    user = lazy(auth)
+    return get_req_for_manager_by_status(uuid.UUID(user["company_id"]))
